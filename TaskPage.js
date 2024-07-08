e2c761b15f5b80e52d0569c499065e43279e834d
sivachandran23 <ksivachandran23@gmail.com>
2024-07-08 18:28:14 +0530

    Add files via upload

---
+++ TaskPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Coffee from '../components/Coffee';
+
+const TaskPage = () => {
+  return (
+    <div>
+      <Coffee />
+    </div>
+  );
+};
+
+export default TaskPage;
